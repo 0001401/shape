1f71bcbdf049f08c3306dfd342ef5604638bf553
Jerry <0001401@naver.com>
2023-10-28 22:07:16 +0900

    feat: content section making

---
+++ src/component/ContentSection/Card.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = ({ width, height }: { width: number; height: number }) => {
+  const cardStyle = {
+    height: `${height}px`,
+    marginTop: "10px",
+    marginBottom: "50px",
+    padding: "20px",
+    width: `${width}px`,
+    borderRadius: "12px",
+    border: "1px solid #E3E3EC",
+    backgroundColor: "#fff",
+  };
+  return (
+    <>
+      <div style={cardStyle}>카드 내용</div>
+    </>
+  );
+};
+
+export default Card;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Header } from "@/component/Header/Header";
 import { MainLayout } from "../component/MainLayout/MainLayout";
-import { Title } from "@/component/ContentSection/Title";
+import ContentSection from "@/component/ContentSection/ContentSection";
 
 const Page = () => {
   return (
@@ -8,7 +8,7 @@ const Page = () => {
       <MainLayout>
         <Header />
         <div className="mb-[100px]" />
-        <Title title={"action"} url={"/action"} />
+        <ContentSection />
       </MainLayout>
     </>
   );
