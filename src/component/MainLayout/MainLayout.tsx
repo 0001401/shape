ae47cbeb0b99c9b0367d2bc529b58c1d389e93a1
Jerry <0001401@naver.com>
2023-10-28 18:48:50 +0900

    feat: header component making

--- src/component/MainLayout/MainLayout.tsx
+++ src/component/MainLayout/MainLayout.tsx
@@ -7,7 +7,7 @@ interface MainLayoutProps {
 export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <section className="bg-[#EAEDF1] w-full h-[1060px] flex justify-center items-center md:py-[40px] lg:py-[40px]">
-      <div className="sm:w-[390px] md:w-[500px] lg-[1200px]  h-full rounded-[50px] p-[70px] bg-[#fff]">
+      <div className="sm:w-[390px] md:w-[500px] lg:w-[1200px]  h-full rounded-[50px] p-[70px] bg-[#fff]">
         {children}
       </div>
     </section>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,15 @@ const Page = () => {
   return (
     <>
       <MainLayout>
-        <p className="text-[30px] font-[700] italic tracking-[-2px]">shape</p>
+        <div className="flex justify-between items-center">
+          <p className="text-[40px] font-[700] italic leading-[170%] tracking-[-2px]">
+            shape
+          </p>
+          <div className="flex">
+            <div className="w-[50px] h-[50px] rounded-[99px] bg-[#E9E9EC] "></div>
+            <div className="w-[150px] h-[50px] rounded-[99px] bg-[#E9E9EC] ml-[20px]"></div>
+          </div>
+        </div>
       </MainLayout>
     </>
   );
